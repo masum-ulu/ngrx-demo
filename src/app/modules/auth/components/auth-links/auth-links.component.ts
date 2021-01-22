ebad37ce46b64effd29bd738cd24dbb804957123
masumulu28 <masum.ulu@hotmail.com>
2021-01-22 22:34:10 +0300

    *auth-links.component refactored. *auth.selectors added viewmodel and selectors

--- src/app/modules/auth/components/auth-links/auth-links.component.ts
+++ src/app/modules/auth/components/auth-links/auth-links.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
+import * as fromAuthSelectors from 'src/app/store/selectors/auth.selectors';
 
 @Component({
   selector: 'app-auth-links',
@@ -6,13 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-links.component.css']
 })
 export class AuthLinksComponent implements OnInit {
-
-  constructor() { }
+  vm$: Observable<fromAuthSelectors.AuthLinksViewModel>;
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+    this.vm$ = this.store.pipe(select(fromAuthSelectors.selectAuthLinksViewModel));
   }
 
   logOut(): void {
-
+    
   }
 }
--- src/app/store/selectors/auth.selectors.ts
+++ src/app/store/selectors/auth.selectors.ts
@@ -4,3 +4,24 @@ import * as fromAuth from '../reducers/auth.reducer';
 export const selectAuthState = createFeatureSelector<fromAuth.State>(
   fromAuth.authFeatureKey
 );
+
+export interface AuthLinksViewModel {
+  isAdmin: boolean;
+  isLoggedin: boolean;
+}
+
+export const selectIsLoggedIn = createSelector(
+  selectAuthState,
+  (state: fromAuth.State): boolean => state.user.id != null
+);
+
+export const selectAuthLinksViewModel = createSelector(
+  selectAuthState,
+  selectIsLoggedIn,
+  (state: fromAuth.State, isLoggedIn: boolean): AuthLinksViewModel => {
+    return {
+      isAdmin: state.user.isadmin,
+      isLoggedin: isLoggedIn
+    };
+  }
+);
